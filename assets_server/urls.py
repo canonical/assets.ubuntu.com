c3a36e3a4c3f0fa3d4c86185327eeac811f3841b
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-26 16:25:12 +0100

    Redirect / to /v1/

--- assets_server/urls.py
+++ assets_server/urls.py
@@ -1,5 +1,6 @@
 # Installed packages
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 # Local code
 from views import Asset, AssetList, AssetJson
@@ -9,6 +10,7 @@ from views import Asset, AssetList, AssetJson
 
 urlpatterns = patterns(
     '',
+    url(r'^$', RedirectView.as_view(url='/v1/', permanent=False)),
     url(r'^v1/$', AssetList.as_view()),
     url(r'^v1/(?P<filename>.+)\.json$', AssetJson.as_view()),
     url(r'^v1/(?P<filename>.+)$', Asset.as_view()),
