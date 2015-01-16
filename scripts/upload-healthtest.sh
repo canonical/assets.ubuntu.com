1d49a3940c0ab033ab9b6cd3645c8269d77c2ae9
Robin Winslow <robin@robinwinslow.co.uk>
2015-01-16 19:38:38 +0000

    Change the healthtest file, uploaded with "upload-healthtest.sh"
    The Canonistack version of swift (at keystone.canonistack.canonical.com)
    for some reason corrupts all text files when trying to download with
    python swiftclient. You might upload a file with the contents "UP",
    but when it is downloaded again, it'll have corrupted characters.
    
    The problem doesn't apply to other installations of swift, so it should
    be fine on Live, but for testing, we can't upload a "chinacachehealthtest.txt"
    or "google-sdfgdsfgdsfgds.html" file without it being corrupted.
    
    So for the healthtest for the deployment process, we've gone with a file
    without an extension. For some reason files without extensions are fine.

--- scripts/upload-healthtest.sh
+++ scripts/upload-healthtest.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname $0)
 
 cd ${BASEDIR}
-./upload-asset.py --url-path ccotp/chinacachehealthtest.txt data/chinacachehealthtest.txt
+./upload-asset.py --url-path healthtest --server-url http://localhost:8080/v1/ data/healthtest
