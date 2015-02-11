6bb83751436f4db9b058b9c32be0e0b829abef4b
Robin Winslow <robin@robinwinslow.co.uk>
2015-02-11 14:44:02 +0000

    Oops - add back the healthtest url_path

--- scripts/upload-healthtest.sh
+++ scripts/upload-healthtest.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname $0)
 
 cd ${BASEDIR}
-./upload-asset.py --server-url http://localhost:8080/v1/ data/healthtest
+./upload-asset.py --url-path healthtest --server-url http://localhost:8080/v1/ data/healthtest
 
 exit_code=$?
 
