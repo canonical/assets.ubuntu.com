9db35394e1c64b99fbdb2c78014ee951fec39466
Robin Winslow <robin@robinwinslow.co.uk>
2015-03-25 21:06:45 +0000

    Update upload-healthtest.sh to work with new filenames

--- scripts/upload-healthtest.sh
+++ scripts/upload-healthtest.sh
@@ -2,8 +2,7 @@
 
 BASEDIR=$(dirname $0)
 
-cd ${BASEDIR}
-./upload-asset.py --url-path healthtest --server-url http://localhost:8080/v1/ data/healthtest
+${BASEDIR}/.upload-asset.py --url-path healthtest --server-url http://localhost:8080/v1/ ${BASEDIR}/data/healthtest
 
 exit_code=$?
 
