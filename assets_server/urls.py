9a9f3eba16368ea5aee3a843584767188f2cf9db
Robin Winslow <robin@robinwinslow.co.uk>
2015-04-14 22:52:21 +0100

    URL paths for managing redirects
    Make use of the new RedirectRecord and RedirectRecords
    view classes, and the new RedirectMapper to allow for
    manipulation of redirects.
    
    These redirects are stored in the "redirects" collection
    in MongoDB, and have a "request_path" and a "target_url".
    
    You can manipulate them with the following URLs and HTTP methods:
    
    /redirects - GET, POST
    /redirects/<request_path> - GET, PUT, DELETE

--- assets_server/urls.py
+++ assets_server/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import patterns, url
 from django.views.generic.base import RedirectView
 
 # Local code
-from views import Asset, AssetList, AssetInfo, Tokens, Token
+from views import (
+    Asset, AssetList, AssetInfo,
+    Tokens, Token,
+    RedirectRecords, RedirectRecord
+)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
@@ -14,6 +18,8 @@ urlpatterns = patterns(
     url(r'^v1/?$', AssetList.as_view()),
     url(r'^v1/tokens/?$', Tokens.as_view()),
     url(r'^v1/tokens/(?P<name>.+)$', Token.as_view()),
+    url(r'^v1/redirects/?$', RedirectRecords.as_view()),
+    url(r'^v1/redirects/(?P<redirect_path>.+)$', RedirectRecord.as_view()),
     url(r'^v1/(?P<file_path>.+)/info$', AssetInfo.as_view()),
     url(r'^v1/(?P<file_path>.+)$', Asset.as_view()),
 )
