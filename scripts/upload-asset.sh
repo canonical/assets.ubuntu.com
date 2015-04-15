01eeebe08500f56adca1b0f9666281e922f3f319
Robin Winslow <robin@robinwinslow.co.uk>
2015-04-15 16:00:10 +0100

    Sensible errors if env not found

--- scripts/upload-asset.sh
+++ scripts/upload-asset.sh
@@ -5,6 +5,12 @@ BASEDIR=$(dirname $0)
 
 # Where is the environment?
 ENVPATH=${VIRTUAL_ENV}
+
 if [ -z ${ENVPATH} ]; then ENVPATH=env; fi
+
+if [ ! -d "${ENVPATH}" ]; then
+    echo "Can't find environment. Do you need to run 'make setup'?"
+    exit 1
+fi
 
 vex --path ${ENVPATH} ${BASEDIR}/.upload-asset.py $@
