b2646b2ca2f42687b68e065b82308faab4a879f2
Will Moggridge <will@moggridge.com>
2017-02-13 14:11:36 +0000

    Remove duplicate operations during image loading
    Remove any duplicate operations specified, using the more-itertools
    function `unique_everseen()`

--- assets_server/lib/processors.py
+++ assets_server/lib/processors.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import os
 
 # Modules
+from more_itertools import unique_everseen
 from pilbox.errors import PilboxError
 from pilbox.image import Image as PilboxImage
 from scour.scour import scourString
@@ -112,6 +113,8 @@ class ImageProcessor:
                 operation = "resize"
 
             operations = operation.split(',')
+            # Remove duplicate operations from list
+            operations = unique_everseen(operations)
 
             for operation in operations:
                 if operation or 'q' in self.options:
