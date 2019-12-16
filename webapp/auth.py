8f4ecc7cfdea6ddf0e9f0df50ac9a71662f5baa6
Robin Winslow <robin@robinwinslow.co.uk>
2019-12-16 18:02:30 +0000

    Restructure tokens code
    - Remove tokens/mappers.py, as it doesn't do much
    - Move tokens views into views.py (use the same structure as we do
      for ubuntu.com)
    - Move CLI commands to commands.py

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,6 +1,10 @@
-from webapp.tokens.mapper import find_token
+from webapp.database import db_session
+from webapp.models import Token
 
 
 def authenticate(token):
     """Check if this authentication token is valid (i.e. exists)"""
-    return True if find_token(token) else False
+
+    return bool(
+        db_session.query(Token).filter(Token.token == token).one_or_none()
+    )
