cb66468c3ed2b7b3bfe5b77e3149d239129ea3f3
Pete <peterfrench94@gmail.com>
2024-10-21 14:28:22 +0200

    refactor: Remove GET method from /create route

--- webapp/routes.py
+++ webapp/routes.py
@@ -82,7 +82,7 @@ def home():
     )
 
 
-@ui_blueprint.route("/create", methods=["GET", "POST"])
+@ui_blueprint.route("/create", methods=["POST"])
 @login_required
 def create():
     created_assets = []
